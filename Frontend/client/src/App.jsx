bb3bf8e581ddf391f79bb5199062c52b3f75ee94
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-11-07 15:53:39 +0600

    Protected route or route guard component

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,12 +1,36 @@
 import { Route, Routes } from "react-router-dom";
 
 import AuthPage from "./Pages/auth";
+import RouteGuard from "./components/Route-Guard";
+import { useContext } from "react";
+import { AuthContext } from "./Context/Auth-Context";
+import InstructorDashboardpage from "./Pages/Instructor";
 
 function App() {
+  const {auth}=useContext(AuthContext)
   return (
     <>
       <Routes>
-        <Route path="/auth" element={<AuthPage />}></Route>
+      <Route
+        path="/auth"
+        element={
+          <RouteGuard
+            element={<AuthPage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+       <Route
+        path="/instructor"
+        element={
+          <RouteGuard
+            element={<InstructorDashboardpage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
       </Routes>
     </>
   );
